127299b4f3f95bc166c9731e44a8d7c0e6a20dfc
Amit <amitlpalandurkar1998@gmail.com>
2023-06-16 15:25:56 +0530

    Wellcome to the Employee wage Computation.

---
+++ EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bridgelab.employwage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.printf("Welcome to the Employee Wage Computation.");
+    }
+}
