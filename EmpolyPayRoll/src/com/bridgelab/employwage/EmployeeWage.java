d619c2b127aee559827d2601f3d50dfac402934a
Amit <amitlpalandurkar1998@gmail.com>
2023-06-16 16:00:09 +0530

    Callculating part time wages.

--- EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
+++ EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
@@ -3,20 +3,25 @@ package com.bridgelab.employwage;
 public class EmployeeWage {
     public static void main(String[] args) {
         //System.out.printf("Welcome to the Employee Wage Computation.");
-        int IS_FULL_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMPLOYEE_RATE_PER_HOUR=10;
         int employeehrs=0;
         int employeewage = 0;
-
-        double empcheck = Math.floor(Math.random()*10)%2;
-
+        int IS_PART_TIME=1;
 
 
-        if (empcheck == IS_FULL_TIME) {
+        double empcheck = Math.floor(Math.random()*10)%3;
+
+
+
+        if (empcheck == IS_PART_TIME) {
             //System.out.printf("Employee is Present.");
-            employeehrs=8;
+            employeehrs=4;
 
-        }else {
+        } else if (empcheck == IS_FULL_TIME) {
+            employeehrs=8;
+        } else {
+
             //System.out.printf("Employee is Absent.");
             employeehrs=0;
         }
