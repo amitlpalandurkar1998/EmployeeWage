f288607dcb40effb0fd96618efea888a9f25978e
Amit <amitlpalandurkar1998@gmail.com>
2023-06-20 01:35:26 +0530

    Assume 20 Working Day per Month.

--- EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
+++ EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
@@ -1,32 +1,43 @@
 package com.bridgelab.employwage;
 
+
 public class EmployeeWage {
+
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMPLOYEE_RATE_PER_HOUR = 10;
+        public static final int TOTAL_WORKING_DAYS = 20;
+        public static final int IS_PART_TIME = 1;
+
+
     public static void main(String[] args) {
         //System.out.printf("Welcome to the Employee Wage Computation.");
-        int IS_FULL_TIME = 2;
-        int EMPLOYEE_RATE_PER_HOUR=10;
         int employeehrs=0;
         int employeewage = 0;
-        int IS_PART_TIME=1;
+        int emoployeetotalwage=0;
+
+        for (int i=1; i<=TOTAL_WORKING_DAYS; i++) {
 
 
-        int empcheck = (int)Math.floor(Math.random()*10)%3;
+            int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        switch (empcheck){
-            case 0:
-                employeehrs=0;
-                break;
-            case 1:
-                employeehrs=4;
-                break;
-            case 2:
-                employeehrs=8;
-                break;
-            default:
-                System.out.println("invalid choice.");
-                break;
+            switch (empcheck) {
+                case 0:
+                    employeehrs = 0;
+                    break;
+                case 1:
+                    employeehrs = 4;
+                    break;
+                case 2:
+                    employeehrs = 8;
+                    break;
+                default:
+                    System.out.println("invalid choice.");
+                    break;
+            }
+            employeewage=employeehrs*EMPLOYEE_RATE_PER_HOUR;
+            System.out.println(employeewage);
+            emoployeetotalwage+=employeewage;
         }
-        employeewage=employeehrs*EMPLOYEE_RATE_PER_HOUR;
-        System.out.println(employeewage);
+        System.out.println(emoployeetotalwage);
     }
 }
