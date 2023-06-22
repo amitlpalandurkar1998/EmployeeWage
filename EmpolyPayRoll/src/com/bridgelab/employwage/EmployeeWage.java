ae3aa7a56021c0b2af55cc42efcffeeda466441d
Amit <amitlpalandurkar1998@gmail.com>
2023-06-23 00:09:18 +0530

    Use Class Method and Class Variables.

--- EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
+++ EmpolyPayRoll/src/com/bridgelab/employwage/EmployeeWage.java
@@ -9,13 +9,15 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
 
     public static final int TOTAL_HOUR = 100;
+    public int employeehrs=0;
+    public int employeewage = 0;
+    public int emoployeetotalwage=0;
+    public int totalEmpHour =0;
 
+    public void calculateEmpWage(){
 
-    public static void main(String[] args) {
         //System.out.printf("Welcome to the Employee Wage Computation.");
-        int employeehrs=0;
-        int employeewage = 0;
-        int emoployeetotalwage=0;
+
 
         for (int i=1; i<=TOTAL_WORKING_DAYS; i++) {
 
@@ -37,11 +39,18 @@ public class EmployeeWage {
                     break;
             }
 
-            if (employeehrs<=TOTAL_HOUR){
+            totalEmpHour+=employeehrs;
+
+            if (totalEmpHour<=TOTAL_HOUR){
                 employeewage = employeehrs * EMPLOYEE_RATE_PER_HOUR;
                 emoployeetotalwage += employeewage;
             }
         }
         System.out.println("Total Employee Wage : "+emoployeetotalwage+" Rs.");
+    }
+
+    public static void main(String[] args) {
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.calculateEmpWage();
     }
 }
